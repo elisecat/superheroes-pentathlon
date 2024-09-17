ee0a2972dfc69123614b4bedccd32c003c0a4ae3
Sara Duran <saraelissad@gmail.com>
2024-09-17 18:40:36 -0500

    feat: add responsive footer with app name, copyright, year, and version

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <header class="p-0 shadow-lg">
-    <div class="mx-auto">
-      <Navbar :routes="navRoutes" />
-    </div>
-  </header>
+  <div class="flex flex-col min-h-screen">
+    <header class="p-0 shadow-lg">
+      <div class="mx-auto">
+        <Navbar :routes="navRoutes" />
+      </div>
+    </header>
 
-  <main :class="[$route.path !== '/home' ? 'container mx-auto py-8' : '']">
-    <RouterView />
-  </main>
+    <main :class="[$route.path !== '/home' ? 'container mx-auto py-8 flex-grow' : 'flex-grow']">
+      <RouterView />
+    </main>
+
+    <footer class="mt-auto">
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import Navbar from './components/shared/Navbar.vue'
+import Footer from '@/components/shared/Footer.vue'
 
 const navRoutes = [
   { path: '/home', name: 'Home', icon: 'fas fa-home' },
   { path: '/heroes', name: 'Heroes', icon: 'fas fa-user' },
-  { path: '/pentathlon', name: 'Pentathlon', icon: 'fas fa-trophy  ' },
-
+  { path: '/pentathlon', name: 'Pentathlon', icon: 'fas fa-trophy' },
 ]
+
+const $route = useRoute()
 </script>
